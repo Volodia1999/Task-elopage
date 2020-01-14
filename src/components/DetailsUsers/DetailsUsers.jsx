fbad59c21826aecaa2292dc27d66d284e5b9ef08
Volodymyr <revuravova@gmail.com>
2020-01-14 16:07:44 +0200

    rewrite from hooks to class

--- src/components/DetailsUsers/DetailsUsers.jsx
+++ src/components/DetailsUsers/DetailsUsers.jsx
@@ -1,107 +1,142 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {userAPI} from '../../userApi/userAPI';
 import uuid from 'uuid';
 import './DetailsUsers.css';
 
-const DetailsUsers = () => {
-    const [getUsers, changeGetUsers] = useState(userAPI);
-    // Change color and show tooltip
-    const [infoIcon, changeInfoIcon] = useState(false);
-    // Open modal
-    const [isOpen, setIsOpen] = useState(false);
-    // Get data from input
-    const [currentUserName, setUserName] = useState('');
-    const [currentUserEmail, setUserEmail] = useState('');
-    const [currentUserPhone, setUserPhone] = useState('');
-
-    const setName = (e) => {
-        setUserName(e.target.value);
+class DetailsUsers extends React.PureComponent {
+    state = {
+        users: [...userAPI],
+        showTooltip: false,
+        isOpen: false,
+        userName: '',
+        userEmail: '',
+        userPhone: ''
     };
 
-    const setEmail = (e) => {
-        setUserEmail(e.target.value);
+    changeColorInfo = () => {
+        this.setState({
+            showTooltip: !this.state.showTooltip
+        })
     };
 
-    const setPhone = (e) => {
-        setUserPhone(e.target.value);
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
     };
 
-    const onSubmit = (e) => {
+    closeModal = () => {
+        this.setState({
+            isOpen: false
+        })
+    };
+
+    setName = (e) => {
+        this.setState({
+            userName: e.target.value
+        })
+    };
+
+    setEmail = (e) => {
+        this.setState({
+            userEmail: e.target.value
+        })
+    };
+
+    setPhone = (e) => {
+        this.setState({
+            userPhone: e.target.value
+        })
+    };
+
+    onSubmit = (e) => {
         e.preventDefault();
+
         const newAccount = {
             id: uuid(),
-            username: currentUserName,
-            email: currentUserEmail,
-            phone: currentUserPhone
+            username: this.state.userName,
+            email: this.state.userEmail,
+            phone: this.state.userPhone
         };
 
-        changeGetUsers([...getUsers, newAccount]);
-        setIsOpen(false);
+        this.setState({
+            users: [...this.state.users, newAccount]
+        });
+
+        this.closeModal();
     };
 
-    return (
-        <>
-            <div className='user-list'>
-                <p>LIST<i onClick={() => changeInfoIcon(!infoIcon)}
-                          className={!infoIcon
-                              ? 'fa fa-info-circle info-icon fa-lg'
-                              : 'fa fa-info-circle fa-lg color-info-icon'}/>
-                    {infoIcon && <span className='tooltip-icon'><i>Account list</i></span>}
-                </p>
-            </div>
-            <div className='details-user-cards'>
-                {getUsers.map((user) => (
-                    <div className='details-user-card' key={user.id}>
-                        <div className='details-user-container'>
-                            <div className='details-user-name'>{user.username}</div>
-                            <div className='details-user-email'>Email: <span>{user.email}</span></div>
-                            <div className='details-user-phone'>Phone: <span>{user.phone}</span></div>
-                            <i onClick={() => {
-                                changeGetUsers(getUsers.filter((u) => u.id !== user.id))
-                            }} className='fa fa-trash-o delete-icon fa-lg'/>
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <div className='add-account'>
-                <button onClick={() => setIsOpen(!isOpen)}><i className='fa fa-plus-circle fa-lg add-icon'/>Add account
-                </button>
-            </div>
-
-            {isOpen &&
-            <div className='modal'>
-                <div className='modal-content'>
-                    <span className='close' onClick={() => setIsOpen(false)}>&times;</span>
-                    <form className='add-user' onSubmit={onSubmit}>
-                        <span className='mt-3'>Username</span>
-                        <input
-                            type='text'
-                            placeholder='Your name...'
-                            onChange={setName} required/>
-                        <span className='mt-3'>Email</span>
-                        <input
-                            type='email'
-                            placeholder='Your email...'
-                            onChange={setEmail} required/>
-                        <span className='mt-3'>Phone</span>
-                        <input
-                            type='text'
-                            placeholder='Your phone...'
-                            onChange={setPhone}/>
-                        <div className='forms-button'>
-                            <div>
-                                <button onClick={() => setIsOpen(false)} className='cancel-button'>Cancel</button>
-                            </div>
-                            <div>
-                                <button className='add-new-account'>Add new account</button>
-                            </div>
-                        </div>
-                    </form>
+    render() {
+        return (
+            <>
+                <div className='user-list'>
+                    <p>LIST<i onClick={this.changeColorInfo}
+                              className={!this.state.showTooltip
+                                  ? 'fa fa-info-circle info-icon fa-lg'
+                                  : 'fa fa-info-circle fa-lg color-info-icon'}/>
+                        {this.state.showTooltip && <span className='tooltip-icon'><i>Account list</i></span>}
+                    </p>
                 </div>
-            </div>
-            }
-        </>
-    )
-};
+                <div className='details-user-cards'>
+                    {this.state.users.map((user) => (
+                        <div className='details-user-card' key={user.id}>
+                            <div className='details-user-container'>
+                                <div className='details-user-name'>{user.username}</div>
+                                <div className='details-user-email'>Email: <span>{user.email}</span></div>
+                                <div className='details-user-phone'>Phone: <span>{user.phone}</span></div>
+                                <i onClick={() => {
+                                    this.setState({
+                                        users: this.state.users.filter((u) => u.id !== user.id)
+                                    })
+                                }} className='fa fa-trash-o delete-icon fa-lg'/>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className='add-account'>
+                    <button onClick={this.toggle}><i className='fa fa-plus-circle fa-lg add-icon'/>Add
+                        account
+                    </button>
+                </div>
+
+                {this.state.isOpen &&
+                <div className='modal'>
+                    <div className='modal-content'>
+                        <span className='close' onClick={this.closeModal}>&times;</span>
+                        <form className='add-user' onSubmit={this.onSubmit}>
+                            <span className='mt-3'>Username</span>
+                            <input
+                                type='text'
+                                placeholder='Your name...'
+                                name='name'
+                                onChange={this.setName} required/>
+                            <span className='mt-3'>Email</span>
+                            <input
+                                type='email'
+                                placeholder='Your email...'
+                                name='useremail'
+                                onChange={this.setEmail} required/>
+                            <span className='mt-3'>Phone</span>
+                            <input
+                                type='text'
+                                placeholder='Your phone...'
+                                name='userphone'
+                                onChange={this.setPhone}/>
+                            <div className='forms-button'>
+                                <div>
+                                    <button onClick={this.closeModal} className='cancel-button'>Cancel</button>
+                                </div>
+                                <div>
+                                    <button className='add-new-account'>Add new account</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                }
+            </>
+        )
+    }
+}
 
 export default DetailsUsers;
