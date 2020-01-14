cb5c6f68d9e281c7749f81e1a8bdeefac3c7a198
Volodymyr <revuravova@gmail.com>
2020-01-14 02:01:01 +0200

    Updates

---
+++ precache-manifest.a2af03401bb8a6830ae1c009d79c4e42.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5791ba9f32c1a485a2f57192b047be",
+    "url": "/Task-elopage/index.html"
+  },
+  {
+    "revision": "b93453d45f5f4136a33f",
+    "url": "/Task-elopage/static/css/2.c6fd14cb.chunk.css"
+  },
+  {
+    "revision": "3de71223111ab93d86bc",
+    "url": "/Task-elopage/static/css/main.7a9b9621.chunk.css"
+  },
+  {
+    "revision": "b93453d45f5f4136a33f",
+    "url": "/Task-elopage/static/js/2.83920ac5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Task-elopage/static/js/2.83920ac5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3de71223111ab93d86bc",
+    "url": "/Task-elopage/static/js/main.40c72a36.chunk.js"
+  },
+  {
+    "revision": "002be64fe3c2b41eab23",
+    "url": "/Task-elopage/static/js/runtime-main.fbfe287d.js"
+  },
+  {
+    "revision": "7099ae8bd4ec17ea6cbaa2ee9a550fe5",
+    "url": "/Task-elopage/static/media/GothamProMedium.7099ae8b.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Task-elopage/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Task-elopage/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Task-elopage/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Task-elopage/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Task-elopage/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
